2126fc8d9040f636cd52892703a2afe72e6448ea
Ayush Jaiswal <112085128+Ayush-Jaiswal25@users.noreply.github.com>
2024-07-11 23:35:34 +0530

    Update backend.js6

--- BACKEND/backend.js
+++ BACKEND/backend.js
@@ -22,9 +22,9 @@ backend.use(express.json());
 backend.use(express.urlencoded({extended: true}))
 
 backend.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://prowork.live'); // Allow requests from any origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow specific HTTP methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+  res.setHeader('Access-Control-Allow-Origin: https://prowork.live'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE'); // Allow specific HTTP methods
+  res.setHeader('Access-Control-Allow-Headers: Content-Type, Authorization'); // Allow specific headers
   next();
 });
 
