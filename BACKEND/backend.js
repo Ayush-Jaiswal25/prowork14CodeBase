e57233e8c76436c2a92857f815ecc61c66e1654f
Ayush Jaiswal <112085128+Ayush-Jaiswal25@users.noreply.github.com>
2024-07-11 23:01:16 +0530

    Update backend.js2

--- BACKEND/backend.js
+++ BACKEND/backend.js
@@ -21,7 +21,7 @@ backend.use(cors())
 backend.use(express.json());
 backend.use(express.urlencoded({extended: true}))
 
-app.use((req, res, next) => {
+backend.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
