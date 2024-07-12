5df285c9748626a8cbc34150753efe80b42e6fe2
Ayush Jaiswal <112085128+Ayush-Jaiswal25@users.noreply.github.com>
2024-07-12 08:23:10 +0530

    Update backend.js12

--- BACKEND/backend.js
+++ BACKEND/backend.js
@@ -24,7 +24,7 @@ backend.use(express.urlencoded({extended: true}))
 backend.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
   res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE'); // Allow specific HTTP methods
-  res.header('Access-Control-Allow-Headers: Content-Type, Authorization'); // Allow specific headers
+  res.header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept'); // Allow specific headers
   next();
 });
 console.log("HEY DONE");
